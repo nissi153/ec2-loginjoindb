4e0c4eb9a77a41c7448fc9f84604bd177af7acaa
nissi153 <36612384+nissi153@users.noreply.github.com>
2023-03-06 17:00:14 +0900

    init commit

---
+++ src/test/java/com/study/springboot/Ex19LoginJoinApplicationTests.java
@@ -0,0 +1,14 @@
+package com.study.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex19LoginJoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("test!!");
+	}
+
+}

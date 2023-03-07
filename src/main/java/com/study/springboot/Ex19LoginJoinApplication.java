dac49b1777852fb77c9756de2e1e0826aee660a5
nissi153 <nissisoft21@gmail.com>
2023-03-07 16:49:22 +0900

    modify

--- src/main/java/com/study/springboot/Ex19LoginJoinApplication.java
+++ src/main/java/com/study/springboot/Ex19LoginJoinApplication.java
@@ -3,6 +3,7 @@ package com.study.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//test
 @SpringBootApplication
 public class Ex19LoginJoinApplication {
 

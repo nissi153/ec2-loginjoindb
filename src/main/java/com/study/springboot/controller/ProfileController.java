e92bcaf06d5d0d4331d9b4a5a070b32c71fd6e90
nissi153 <nissisoft21@gmail.com>
2023-03-14 12:24:37 +0900

    commit

--- src/main/java/com/study/springboot/controller/ProfileController.java
+++ src/main/java/com/study/springboot/controller/ProfileController.java
@@ -13,17 +13,16 @@ import java.util.List;
 public class ProfileController {
     private final Environment env;
 
+    //현재 어떤 포트로 돌아가는지 확인용. set1 = 8081, set2 = 8082
     @GetMapping("/profile")
     public String profile() {
-        List<String> profiles = Arrays.asList(env.getActiveProfiles());
-        List<String> realProfiles = Arrays.asList("real", "real1", "real2");
+        //현재 동작중인 프로파일의 이름을 반환
+        return Arrays.stream(env.getActiveProfiles()).findFirst().orElse("");
+    }
 
-        String defaultProfile = profiles.isEmpty() ? "default" : profiles.get(0);
-        System.out.println("defaultProfile:"+defaultProfile);
-
-        return profiles.stream()
-                .filter(realProfiles::contains)
-                .findAny()
-                .orElse(defaultProfile);
+    //무중단배포 테스트를 위한 version 확인용. 새로고침하면 return 값이 자동으로 바뀌는지
+    @GetMapping("/version")
+    public String checkVersion() {
+        return "ver2";
     }
 }

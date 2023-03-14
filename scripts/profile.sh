2331a4cdfc896e3b8de70c549a16fb5d2b66227d
nissi153 <nissisoft21@gmail.com>
2023-03-14 13:22:41 +0900

    commit

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -12,7 +12,7 @@ function find_idle_profile(){
      CUREENT_PROFILE=$(curl -s http://localhost/profile)
  fi
 
- if [ $(CUREENT_PROFILE) == real1 ]
+ if [ "$(CUREENT_PROFILE)" == real1 ]
  then
      IDLE_PROFILE=real2
  else
@@ -26,7 +26,7 @@ function find_idle_profile(){
 function find_idle_port() {
    IDLE_PROFILE=$(find_idle_profile)
 
-   if [ ${IDLE_PROFILE} == real1 ]
+   if [ "${IDLE_PROFILE}" == real1 ]
    then
        echo "8081"
    else
